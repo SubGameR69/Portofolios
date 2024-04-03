5372cdd4ce7aae66fdf2d8d8118e97f64f594d8b
SubGameR69e <azjoker333@gmail.com>
2024-04-03 22:49:41 +0000

    About Section and sticky navbar

--- Responsive Protfolio/js/script.js
+++ Responsive Protfolio/js/script.js
@@ -1,21 +1,13 @@
-/*========== menu icon navbar ==========*/
-let menuIcon = document.querySelector('#menu-icon');
-let navbar = document.querySelector('.navbar');
-
-
-/*========== scroll sections active link ==========*/
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-
-
     /*========== sticky navbar ==========*/
+window.onscroll = () => {
+    let header = document.querySelector(".header")
+
+    header.classList.toggle("sticky", window.scrollY > 100);
+}
+
+/*========== remove menu icon navbar when click navbar link (scroll) ==========*/
 
 
-    /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
-
-};
 
 
 /*========== swiper ==========*/
