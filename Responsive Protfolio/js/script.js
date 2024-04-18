384ff43cb2d847f785dc089b3793cdbacff0f40d
SubGameR69e <azjoker333@gmail.com>
2024-04-18 23:07:55 +0100

    Menu-Icon and Media Screen

--- Responsive Protfolio/js/script.js
+++ Responsive Protfolio/js/script.js
@@ -22,6 +22,10 @@ window.onscroll = () => {
     let header = document.querySelector(".header")
 
     header.classList.toggle("sticky", window.scrollY > 100);
+    /*========== remove menu icon navbar ==========*/
+    menuIcon.classList.remove("bx-x");
+    navbar.classList.remove("active");
+
 }
 
 /*========== remove menu icon navbar when click navbar link (scroll) ==========*/
@@ -49,3 +53,13 @@ var swiper = new Swiper(".mySwiper", {
 
 
 /*========== scroll reveal ==========*/
+
+
+/*========== menu icon navbar ==========*/
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle("bx-x");
+    navbar.classList.toggle("active");
+}
