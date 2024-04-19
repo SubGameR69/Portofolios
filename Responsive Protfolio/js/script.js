04751757ae04b8abb513dbe421a1fa0da550133d
SubGameR69e <azjoker333@gmail.com>
2024-04-19 01:19:50 +0100

    DarkMode

--- Responsive Protfolio/js/script.js
+++ Responsive Protfolio/js/script.js
@@ -28,10 +28,6 @@ window.onscroll = () => {
 
 }
 
-/*========== remove menu icon navbar when click navbar link (scroll) ==========*/
-
-
-
 
 /*========== swiper ==========*/
 var swiper = new Swiper(".mySwiper", {
@@ -50,10 +46,25 @@ var swiper = new Swiper(".mySwiper", {
 });
 
 /*========== dark light mode ==========*/
+let darkModeIcon = document.querySelector("#darkMode-icon");
 
+darkModeIcon.onclick = () => {
+    darkModeIcon.classList.toggle("bx-sun");
+    document.body.classList.toggle("dark-mode");
+}
 
 /*========== scroll reveal ==========*/
+ScrollReveal ({
+    // reset: true,
+    distance: "80px",
+    duration: 2000,
+    delay: 200,
+});
 
+ScrollReveal().reveal(".home-content, .heading", {origin:"top"});
+ScrollReveal().reveal(".home-img img, .services-container, .portfolio-box, .testimonial-wrapper, .contact form", {origin:"bottom"});
+ScrollReveal().reveal(".home-content h1, .about-img img", {origin:"left"});
+ScrollReveal().reveal(".home-content h3, .home-content p, .about-content", {origin:"right"});
 
 /*========== menu icon navbar ==========*/
 let menuIcon = document.querySelector("#menu-icon");
